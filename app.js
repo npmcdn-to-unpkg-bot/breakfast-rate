e45269d45e73472319fecd2562f829ec1f8c5a23
Thomas-Ko <tomkolodzi@gmail.com>
2016-03-24 13:12:33 -0400

    Create mongo database / Edit recipe create route to work with database

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
 var express 			= require("express"),
-	bodyParser 			= require('body-parser');
+	bodyParser 			= require('body-parser'),
+	mongoose 			= require('mongoose');
 
 var app = express();
+
+
+mongoose.connect('mongodb://localhost/breakfast-rate');
 
 app.use(bodyParser.urlencoded({ extended: true }));	//tells express to use bodyParser
 
 app.set("view engine", "ejs");
 
 
-var recipes = [
-	{
-		name: "Hot-Sauce Omelette",
-		image: "https://farm4.staticflickr.com/3094/2851359698_7a5b0c28e5.jpg",
-		description: "This omelette packs a whole lot of flavor with a kick of heat."
-	},
-	{
-		name: "Divine Waffles",
-		image: "https://farm1.staticflickr.com/48/144982133_9ec847fcf4.jpg",
-		description: "Fresh waffles and vanilla ice cream make for a heavenly combination."
-	},
-	{
-		name: "Perfect Bacon",
-		image: "https://farm7.staticflickr.com/6158/6175755733_b2932d7838.jpg",
-		description: "You can't go wrong with bacon, especially perfectly cooked bacon." 
-	},
-];
+// var recipes = [
+// 	{
+// 		name: "Hot-Sauce Omelette",
+// 		image: "https://farm4.staticflickr.com/3094/2851359698_7a5b0c28e5.jpg",
+// 		description: "This omelette packs a whole lot of flavor with a kick of heat."
+// 	},
+// 	{
+// 		name: "Divine Waffles",
+// 		image: "https://farm1.staticflickr.com/48/144982133_9ec847fcf4.jpg",
+// 		description: "Fresh waffles and vanilla ice cream make for a heavenly combination."
+// 	},
+// 	{
+// 		name: "Perfect Bacon",
+// 		image: "https://farm7.staticflickr.com/6158/6175755733_b2932d7838.jpg",
+// 		description: "You can't go wrong with bacon, especially perfectly cooked bacon." 
+// 	},
+// ];
+
+//MONGOOSE / MODEL CONFIG
+var recipeSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Recipe = mongoose.model("Recipe", recipeSchema);
+
+// Recipe.create(
+// 	{
+// 		name: "Perfect Bacon",
+// 		image: "https://farm7.staticflickr.com/6158/6175755733_b2932d7838.jpg",
+// 		description: "You can't go wrong with bacon, especially perfectly cooked bacon.",
+// 	}, function(err, recipe){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("New Recipe:");
+// 			console.log(recipe);
+// 		}
+// 	}
+// );
+
 
 //LANDING PAGE
 app.get("/", function(req,res){
@@ -37,7 +67,14 @@ app.get("/", function(req,res){
 
 //INDEX
 app.get("/recipes", function(req,res){
-	res.render("recipes", {recipes:recipes});
+	//get all campgrounds from database
+	Recipe.find({}, function(err,recipes){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("recipes", {recipes:recipes});
+		}
+	});
 });
 
 //NEW
@@ -58,11 +95,17 @@ app.post("/recipes", function(req,res){
 		description: description
 	};
 
-	recipes.push(newRecipe);
-
-	//redirect back to recipes page
-	res.redirect("recipes");
+	//create new recipe and save to database
+	Recipe.create(newRecipe, function(err, createdRecipe){
+		if(err){
+			console.log(err);
+		} else {
+			//redirect back to recipes page
+			res.redirect("recipes");
+		}
+	});
 });
+
 
 
 app.listen(process.env.PORT || 3000, function(){
