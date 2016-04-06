4fe12e9e5dcd8e9f5a92cf3d5fabe3acb0fcb19e
Thomas-Ko <tomkolodzi@gmail.com>
2016-04-06 12:48:20 -0400

    Add index of li to span text

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,32 @@ $grid.imagesLoaded().progress( function() {
   $grid.masonry();
 });  
 
+
 $("#addInstruction").on("click", function(){
-	$("#instructionHolder").append('<textarea name="instructions" class="form-control" rows="3"></textarea>');
+	$("#instructionHolder>ol").append('<li>Step <span class="step"></span><textarea name="instructions" class="form-control" rows="3"></textarea><span class="deleteInstruction">X</span></li>');
+	setInstructionNumber();
 });
+
+
+
+
+function setInstructionNumber(){
+	$("span.step").each(function(){
+		$(this).text($(this).parent().index()+1);
+	});
+}
+
+setInstructionNumber();
+
+// $("span.deleteInstruction").on("click", function(){
+// 	$(this).parent().remove();
+// });
+
+
+// $("span.deleteInstruction").each(function(){
+// 	console.log("hey");
+// });
+
+// $("#instructionHolder>ol>>li>span").text("HELLO");
+
+// "$instructionHolder>li".value()
