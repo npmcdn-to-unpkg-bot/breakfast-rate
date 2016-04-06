6a8b61ad15134e8ee97f0d27b2a39e02169f2a62
Thomas-Ko <tomkolodzi@gmail.com>
2016-04-06 13:22:42 -0400

    Allow user to delete an instruction when adding new recipe
    - new recipe form still needs styling

--- public/js/main.js
+++ public/js/main.js
@@ -8,14 +8,9 @@ $grid.imagesLoaded().progress( function() {
   $grid.masonry();
 });  
 
-
-$("#addInstruction").on("click", function(){
-	$("#instructionHolder>ol").append('<li>Step <span class="step"></span><textarea name="instructions" class="form-control" rows="3"></textarea><span class="deleteInstruction">X</span></li>');
-	setInstructionNumber();
-});
-
-
-
+/*====================
+	NEW RECIPE FORM
+====================*/
 
 function setInstructionNumber(){
 	$("span.step").each(function(){
@@ -23,17 +18,23 @@ function setInstructionNumber(){
 	});
 }
 
+//allows the span with class "delete-instruction" to delete the instruction when clicked
+function setDeleteInstruction(){
+	$("span.delete-instruction").click(function(){
+		var that = $(this).parent();
+		that.fadeOut( function() {
+			that.remove();
+			setInstructionNumber();
+		});
+	});
+}
+
+$("#addInstruction").on("click", function(){
+	// $("#instructionHolder>ol").append('<li>Step <span class="step"></span><textarea name="instructions" class="form-control" rows="3"></textarea><span class="setDeleteInstruction">X</span></li>');
+	$('<li>Step <span class="step"></span><textarea name="instructions" class="form-control" rows="3"></textarea><span class="delete-instruction">X</span></li>').hide().appendTo("#instructionHolder>ol").fadeIn();
+	setInstructionNumber();
+	setDeleteInstruction();
+});
+
 setInstructionNumber();
-
-// $("span.deleteInstruction").on("click", function(){
-// 	$(this).parent().remove();
-// });
-
-
-// $("span.deleteInstruction").each(function(){
-// 	console.log("hey");
-// });
-
-// $("#instructionHolder>ol>>li>span").text("HELLO");
-
-// "$instructionHolder>li".value()
+setDeleteInstruction();
